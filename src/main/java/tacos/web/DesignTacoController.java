6afdfd544a2e0d8b0d5029ed31da69c218ee6b0e
crystal <2337jeong@gmail.com>
2020-08-17 14:34:43 +0900

    20200817

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,8 @@ import tacos.data.UserRepository;
 
 @Slf4j
 @Controller
-@RequestMapping("/design")
+@RequestMapping(path="/design", produces="application/json")
+@CrossOrigin(origins="*")
 @SessionAttributes("order")
 public class DesignTacoController {
 	
